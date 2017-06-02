c9c03a6bcbf52c141e987a2088ab96a9602ef981
GuillaumeR19 <g.rouge19@gmail.com>
2017-06-02 11:11:55 +0200

    création de la page d'accueil

---
+++ index3.1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+   <form method="POST" action="traitement_upload3.1.php" enctype="multipart/form-data"> 
+     <!-- le enctype="multipart/form-data" (method="post" est obligatoire) est indispensable pour envoyer des fichiers -->
+      <input type="file" name="fichier">
+      <button type="submit">Envoyer</button>
+       
+       
+       
+   </form>
+      
+    <script src=""></script>
+</body>
+</html>
